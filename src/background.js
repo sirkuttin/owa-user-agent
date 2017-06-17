1e30b7cfd9fe66320501671a20eec8675aa95b7c
Jamieson Becker <jamieson@jamiesonbecker.com>
2017-06-17 17:58:46 -0500

    Closes part of #3

--- src/background.js
+++ src/background.js
@@ -5,12 +5,14 @@ var requestFilter = {
 chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
 	var headers = details.requestHeaders;
 	for(var i = 0, l = headers.length; i < l; ++i) {
-		if( headers[i].name == 'User-Agent' ) {
+		if( headers[i].name === 'User-Agent' ) {
 			break;
 		}
 	}
 	if(i < headers.length) {
-		headers[i].value = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36";
+            // headers[i].value = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36";
+            // change for issue #3
+            headers[i].value = headers[i].value.replace(/(Mozilla\/\d+\.\d+ )\([^\)]+\)/, "\$1(Windows NT 6.1; WOW64)");
 	}
 	return {requestHeaders: headers};
 }, requestFilter, ['requestHeaders','blocking']);
